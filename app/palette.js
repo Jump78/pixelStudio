9bfc267afbf41888b9e19d0290c9a6ae2933f92d
Clement Teboul <teboul.clement@gmail.com>
2016-10-07 16:28:23 +0200

    Palette de couleur opérationel

--- app/palette.js
+++ app/palette.js
@@ -1,13 +1,38 @@
 pixelStudio.palette = {
-	initColor: function (colors) {
 
-		var clone = $('#paletteColor li:first').detach();
+	colors: [],
+
+	colorSelected: null,
+
+	initColor: function (colors) {
+		this.colors = colors;
+
+		var $clone = $('#paletteColor li').detach();
 		
-		for (var i = 0; i < colors.length; i++) {
-			$(clone).css('background-color',colors[i].to_string());
-			$(clone).prependTo('#paletteColor');
-			clone = $('#paletteColor li:first').clone();
+		for (var i = colors.length-1; i >= 0; i--) {
+			let $c = $clone.clone();
+			let color = this.colors[i];
+			color.id = i;
+			$c.css('background-color',colors[i].to_string());
+			$c.attr('title',colors[i].name);
+			$c.prependTo('#paletteColor');
 		}
+		
+		this.selectColor(colors[0]);
+
+		$('#paletteColor li').eq(colors[colors.length-1]).addClass('selected');
+		
+		$('#paletteColor').on('click','li',function(e){
+			var index = $(this).index();
+			pixelStudio.palette.selectColor(pixelStudio.palette.colors[index])
+		})
+	
+	},
+
+	selectColor(color){
+		this.colorSelected = color;
+		$('#paletteColor li').removeClass('selected');
+		$('#paletteColor li').eq(color.id).addClass('selected');
 
 	}
 }
